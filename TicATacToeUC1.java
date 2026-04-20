c5950a107a37b425dfef51dfa194538710dc6323
ritiiii-18 <ritishadixit17@gmail.com>
2026-04-20 14:11:33 +0530

    Add files via upload

---
+++ TicATacToeUC1.java
@@ -0,0 +1,23 @@
+public class TicATacToeUC1 {
+
+    static char [][] board = new char[3][3];
+
+    public static void main(String[] args){
+        intitializeBoard();
+        printBoard();
+    }
+
+    static void intitializeBoard() {
+    }
+
+    static void printBoard(){
+        System.out.println(" -----------");
+        for(int row=0; row<3; row++){
+            
+        }
+    }
+
+
+
+    
+}
